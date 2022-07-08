c2d34fc0eb2faac315a11436ad74ca36c3215fd5
teddzyb <teddybartletg88@gmail.com>
2022-07-08 19:58:48 +0800

    Add files for menu page

---
+++ pages/menu.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Journeyer</title>
+        <meta name="description" content="Journeyer of the Realms is a tabletop card-collecting strategy game developed by Paper Kiwi." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <nav>
+      </nav>
+      <main className="h-screen flex flex-col justify-center">
+      </main>
+    </div>
+  )
+}
