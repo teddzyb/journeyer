51587a19d8edbcb07ea567ce761670038fb51ea6
teddzyb <teddybartletg88@gmail.com>
2022-07-10 13:31:17 +0800

    Add menu items

--- pages/menu.js
+++ pages/menu.js
@@ -3,6 +3,16 @@ import Head from 'next/head'
 import TopMenuBar from '../components/top-menu-bar'
 
 export default function Menu() {
+
+  let menuItems = [
+    { name: "Campaign", href: null },
+    { name: "Ranked", href: null },
+    { name: "Custom", href: null },
+    { name: "Practice", href: null },
+    { name: "Shop", href: null },
+    { name: "Sign Out", href: null },
+  ]
+
   return (
     <div>
       <Head>
@@ -13,7 +23,20 @@ export default function Menu() {
       <nav className="sticky top-0">
         <TopMenuBar />
       </nav>
-      <main className="h-screen flex flex-col justify-center">
+      <main className="h-screen flex flex-row justify-center pb-20">
+        <div className="w-2/5 m-10 overflow-y-auto">
+          <div className="grid grid-flow-row-dense gap-10 m-12">
+            {menuItems.map((item, index) =>
+              <div
+                key={index}
+                className="text-2xl drop-shadow-lg cursor-default select-none w-fit 
+                hover:text-teal-200 hover:scale-105 transition ease-in-out duration-150">
+                {item.name.toUpperCase()}
+              </div>)}
+          </div>
+        </div>
+        <div className="rounded-2xl bg-black/[.15] shadow-md w-3/5 m-10 overflow-y-auto">
+        </div>
       </main>
     </div>
   )
