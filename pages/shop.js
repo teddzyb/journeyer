2f80cf5f183cc23b660f06ff9db08f5a09989d7d
teddzyb <teddybartletg88@gmail.com>
2022-07-13 15:29:31 +0800

    Add custom scrollbar to panels

--- pages/shop.js
+++ pages/shop.js
@@ -27,10 +27,10 @@ export default function Shop() {
         <TopMenuBar />
       </nav>
       <main className="h-screen flex flex-row justify-center pb-20">
-        <div className="flex flex-col rounded-2xl bg-black/[.15] shadow-md w-2/5 m-10 overflow-y-clip">
-          <div className="bg-black/[.15] text-xl text-center select-none p-3 pt-4">SHOP</div>
+        <div className="flex flex-col rounded-2xl bg-translucent shadow-md w-2/5 m-10 overflow-y-clip">
+          <div className="bg-translucent text-xl text-center select-none p-3 pt-4">SHOP</div>
           <Tab.Group>
-            <Tab.List as="div" className="flex justify-center items-center bg-black/10 border-t border-b border-black/[.15] p-2">
+            <Tab.List as="div" className="flex justify-center items-center bg-black/10 border-t border-b border-translucent p-2">
               {tabs.map((item, index) =>
                 <Tab
                   as="button"
@@ -45,21 +45,21 @@ export default function Shop() {
                 </Tab>
               )}
             </Tab.List>
-            <Tab.Panels as="div" className="overflow-y-auto">
+            <Tab.Panels as="div" className="scrollbar-thin scrollbar-thumb-translucent scrollbar-track-transparent">
               <Tab.Panel as="div" className="grid grid-cols-2">
-                <div className="flex flex-col items-center border border-black/[.15] py-6">
+                <div className="flex flex-col items-center border border-translucent py-6">
                   <div className="bg-neutral-300 rounded-md drop-shadow-md w-48 h-64"></div>
                   <div className="z-10 bg-neutral-500 rounded-sm shadow-sm -mt-6 h-10 w-[14.5rem]"></div>
                 </div>
-                <div className="flex flex-col items-center border border-black/[.15] py-6">
+                <div className="flex flex-col items-center border border-translucent py-6">
                   <div className="bg-neutral-300 rounded-md drop-shadow-md w-48 h-64"></div>
                   <div className="z-10 bg-neutral-500 rounded-sm shadow-sm -mt-6 h-10 w-[14.5rem]"></div>
                 </div>
-                <div className="flex flex-col items-center border border-black/[.15] py-6">
+                <div className="flex flex-col items-center border border-translucent py-6">
                   <div className="bg-neutral-300 rounded-md drop-shadow-md w-48 h-64"></div>
                   <div className="z-10 bg-neutral-500 rounded-sm shadow-sm -mt-6 h-10 w-[14.5rem]"></div>
                 </div>
-                <div className="flex flex-col items-center border border-black/[.15] py-6">
+                <div className="flex flex-col items-center border border-translucent py-6">
                   <div className="bg-neutral-300 rounded-md drop-shadow-md w-48 h-64"></div>
                   <div className="z-10 bg-neutral-500 rounded-sm shadow-sm -mt-6 h-10 w-[14.5rem]"></div>
                 </div>
@@ -73,8 +73,8 @@ export default function Shop() {
             </Tab.Panels>
           </Tab.Group>
         </div>
-        <div className="flex flex-col rounded-2xl bg-black/[.15] shadow-md w-3/5 m-10 overflow-y-auto">
-          <div className="bg-black/[.15] text-xl text-center select-none p-3 pt-4">STARTER DECK #1</div>
+        <div className="flex flex-col rounded-2xl bg-translucent shadow-md w-3/5 m-10 overflow-y-auto">
+          <div className="bg-translucent text-xl text-center select-none p-3 pt-4">STARTER DECK #1</div>
 
         </div>
       </main>
