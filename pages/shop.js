9e45d02246140252d43456645df33a5360622738
teddzyb <teddybartletg88@gmail.com>
2022-07-13 12:58:25 +0800

    Add initial design for "decks" shop panel

--- pages/shop.js
+++ pages/shop.js
@@ -5,7 +5,7 @@ import { Tab } from '@headlessui/react'
 import TopMenuBar from '../components/top-menu-bar'
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ")
 }
 
 export default function Shop() {
@@ -27,7 +27,7 @@ export default function Shop() {
         <TopMenuBar />
       </nav>
       <main className="h-screen flex flex-row justify-center pb-20">
-        <div className="flex flex-col rounded-2xl bg-black/[.15] shadow-md w-2/5 m-10 overflow-y-auto">
+        <div className="flex flex-col rounded-2xl bg-black/[.15] shadow-md w-2/5 m-10 overflow-y-clip">
           <div className="bg-black/[.15] text-xl text-center select-none p-3 pt-4">SHOP</div>
           <Tab.Group>
             <Tab.List as="div" className="flex justify-center items-center bg-black/10 border-t border-b border-black/[.15] p-2">
@@ -37,19 +37,39 @@ export default function Shop() {
                   key={index}
                   className={({ selected }) =>
                     classNames(
-                      "align-middle rounded-lg w-full mx-2 pb-1 pt-2",
-                      "outline-none",
-                      selected ? "bg-teal-600 shadow focus:outline-1 focus:outline-offset-0 focus:outline-teal-500" : "hover:bg-teal-500/10"
+                      "outline-none align-middle rounded-lg w-full mx-2 pb-1 pt-2",
+                      selected ? "bg-teal-600 shadow focus:outline-2 focus:outline-offset-0 focus:outline-teal-500" : "hover:bg-teal-500/10"
                     )
                   }>
                   {item.name.toUpperCase()}
                 </Tab>
               )}
             </Tab.List>
-            <Tab.Panels>
-              <Tab.Panel>Content 1</Tab.Panel>
-              <Tab.Panel>Content 2</Tab.Panel>
-              <Tab.Panel>Content 3</Tab.Panel>
+            <Tab.Panels as="div" className="overflow-y-auto">
+              <Tab.Panel as="div" className="grid grid-cols-2">
+                <div className="flex flex-col items-center border border-black/[.15] py-6">
+                  <div className="bg-neutral-300 rounded-md drop-shadow-md w-48 h-64"></div>
+                  <div className="z-10 bg-neutral-500 rounded-sm shadow-sm -mt-6 h-10 w-[14.5rem]"></div>
+                </div>
+                <div className="flex flex-col items-center border border-black/[.15] py-6">
+                  <div className="bg-neutral-300 rounded-md drop-shadow-md w-48 h-64"></div>
+                  <div className="z-10 bg-neutral-500 rounded-sm shadow-sm -mt-6 h-10 w-[14.5rem]"></div>
+                </div>
+                <div className="flex flex-col items-center border border-black/[.15] py-6">
+                  <div className="bg-neutral-300 rounded-md drop-shadow-md w-48 h-64"></div>
+                  <div className="z-10 bg-neutral-500 rounded-sm shadow-sm -mt-6 h-10 w-[14.5rem]"></div>
+                </div>
+                <div className="flex flex-col items-center border border-black/[.15] py-6">
+                  <div className="bg-neutral-300 rounded-md drop-shadow-md w-48 h-64"></div>
+                  <div className="z-10 bg-neutral-500 rounded-sm shadow-sm -mt-6 h-10 w-[14.5rem]"></div>
+                </div>
+              </Tab.Panel>
+              <Tab.Panel>
+                Content 2
+              </Tab.Panel>
+              <Tab.Panel>
+                Content 3
+              </Tab.Panel>
             </Tab.Panels>
           </Tab.Group>
         </div>
