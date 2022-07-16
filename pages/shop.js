b3a853897bbcb54ac906f9e478ec2d314eef9e40
teddzyb <teddybartletg88@gmail.com>
2022-07-16 19:59:31 +0800

    Add transitions & toggle card inspect

--- pages/shop.js
+++ pages/shop.js
@@ -5,7 +5,7 @@ import Link from 'next/link'
 import { useState } from 'react'
 
 // Components
-import { Tab, Disclosure } from '@headlessui/react'
+import { Tab, Disclosure, Transition } from '@headlessui/react'
 import TopMenuBar from '../components/top-menu-bar'
 import Card from '../components/card'
 
@@ -24,6 +24,8 @@ export default function Shop() {
 
   const [selectedTab, setSelectedTab] = useState(0)
   const [selectedItem, setSelectedItem] = useState(0)
+  const [selectedCard, setSelectedCard] = useState(null)
+  let cardSelected = (selectedCard != null ? true : false)
 
   let tabs = [
     { name: "Decks", disabled: false },
@@ -78,7 +80,10 @@ export default function Shop() {
                 {Array.from(Array(8).keys()).map(item =>
                   <button
                     key={item}
-                    onClick={() => setSelectedItem(item)}
+                    onClick={() => {
+                      setSelectedItem(item)
+                      setSelectedCard(null)
+                    }}
                     className="flex flex-col items-center border border-translucent py-6 hover:bg-translucent/10">
                     <Card key={item} size="md" className={item === selectedItem && "ring-4 ring-teal-500"} />
                     <div className="flex justify-center items-center z-10 bg-teal-800 rounded-sm shadow-sm 
@@ -97,77 +102,104 @@ export default function Shop() {
             </Tab.Panels>
           </Tab.Group>
         </div>
-        <div className="flex flex-col rounded-2xl bg-translucent shadow-md w-3/5 m-10 ml-5 overflow-y-auto">
+        <div className="flex flex-col rounded-2xl bg-translucent shadow-md w-3/5 m-10 ml-5 overflow-y-hidden">
           <div className="bg-translucent text-xl text-center select-none p-3 pt-4">STARTER DECK #{selectedItem}</div>
           {selectedTab === 0 &&
             <>
-              <div className="grid grid-cols-4 grow gap-y-10 p-7 scrollbar-thin scrollbar-thumb-translucent scrollbar-track-transparent">
-                {/* TODO: inspect card */}
+              <Transition
+                show={cardSelected}
+                enter="transition-all duration-500"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                className="flex justify-center h-full pt-16"
+              >
+                <div className="">
+                  <Card size="lg">{selectedCard}</Card>
+                </div>
+              </Transition>
+              <Transition
+                show={!cardSelected}
+                enter="transition-all duration-500"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                className="grid grid-cols-4 grow gap-y-10 h-full p-7 scrollbar-thin scrollbar-thumb-translucent scrollbar-track-transparent"
+              >
                 {Array.from(Array(30).keys()).map(item =>
                   <div key={item} className="flex flex-col items-center">
-                    <button>
-                      <Card key={item} size="sm" className="hover:bg-amber-100" />
+                    <button onClick={() => setSelectedCard(item)}>
+                      <Card key={item} size="sm" className="hover:bg-amber-100">{item}</Card>
                     </button>
                   </div>
                 )}
-              </div>
-              <Disclosure defaultOpen as="div" className="bg-translucent">
-                {({ open }) => (
-                  <>
-                    <div className="flex justify-between w-full">
-                      <div className="flex flex-col items-start gap-1 text-lg mx-7 my-4 pb-1">
-                        Starter Deck #{selectedItem}
-                        <div className="flex gap-4 text-sm">
-                          <div className="flex gap-1">
-                            <Image
-                              priority
-                              src={silverCoin}
-                              height={20}
-                              width={20}
-                              alt="Silver"
-                              draggable="false"
-                            />
-                            <span className="line-through text-white/70">13,600</span> 12,550
-                          </div>
-                          <div className="flex gap-1">
-                            <Image
-                              priority
-                              src={cardLayer}
-                              height={20}
-                              width={20}
-                              alt="Cards"
-                              className="scale-90"
-                              draggable="false"
-                            />
-                            7/30
+              </Transition>
+              <Transition
+                show={!cardSelected}
+                enter="transition-all duration-500"
+                enterFrom="translate-y-full opacity-0"
+                enterTo="translate-y-0 opacity-100"
+                leave="transition-all duration-500"
+                leaveFrom="translate-y-0 opacity-100"
+                leaveTo="translate-y-full opacity-0"
+                className="bg-translucent"
+              >
+                <Disclosure defaultOpen>
+                  {({ open }) => (
+                    <>
+                      <div className="flex justify-between w-full">
+                        <div className="flex flex-col items-start gap-1 text-lg mx-7 my-4 pb-1">
+                          Starter Deck #{selectedItem}
+                          <div className="flex gap-4 text-sm">
+                            <div className="flex gap-1">
+                              <Image
+                                priority
+                                src={silverCoin}
+                                height={20}
+                                width={20}
+                                alt="Silver"
+                                draggable="false"
+                              />
+                              <span className="line-through text-white/70">13,600</span> 12,550
+                            </div>
+                            <div className="flex gap-1">
+                              <Image
+                                priority
+                                src={cardLayer}
+                                height={20}
+                                width={20}
+                                alt="Cards"
+                                className="scale-90"
+                                draggable="false"
+                              />
+                              7/30
+                            </div>
                           </div>
                         </div>
-                      </div>
-                      <div className="flex">
-                        <div className="flex items-center gap-5">
-                          <button className="btn-primary h-11 w-44 pt-1">Buy Deck</button>
+                        <div className="flex">
+                          <div className="flex items-center gap-5">
+                            <button className="btn-primary h-11 w-44 pt-1">Buy Deck</button>
+                          </div>
+                          <Disclosure.Button className="flex p-5 px-8">
+                            <FontAwesomeIcon
+                              alt="Toggle Details"
+                              icon={open ? faChevronDown : faChevronUp}
+                              className="text-2xl"
+                              draggable="false"
+                            />
+                          </Disclosure.Button>
                         </div>
-                        <Disclosure.Button className="flex p-5 px-8">
-                          <FontAwesomeIcon
-                            alt="Toggle Details"
-                            icon={open ? faChevronDown : faChevronUp}
-                            className="text-2xl"
-                            draggable="false"
-                          />
-                        </Disclosure.Button>
-                      </div>
 
-                    </div>
-                    <Disclosure.Panel
-                      className="max-h-28 pr-7 mx-7 mb-5 -mt-1 scrollbar-thin
-                    scrollbar-thumb-translucent scrollbar-track-transparent">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Donec euismod, nisl eget consectetur sagittis, nisl nunc consectetur nisi,
-                      euismod aliquet nisi nisl euismod.
-                    </Disclosure.Panel>
-                  </>
-                )}
-              </Disclosure>
+                      </div>
+                      <Disclosure.Panel
+                        className="max-h-28 pr-7 mx-7 mb-5 -mt-1 scrollbar-thin
+                        scrollbar-thumb-translucent scrollbar-track-transparent">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Donec euismod, nisl eget consectetur sagittis, nisl nunc consectetur nisi,
+                        euismod aliquet nisi nisl euismod.
+                      </Disclosure.Panel>
+                    </>
+                  )}
+                </Disclosure>
+              </Transition>
             </>
           }
           {selectedTab === 1 &&
