9696996cd381b48e36d035a5e94a78b5be6fa4dd
teddzyb <teddybartletg88@gmail.com>
2022-07-26 22:15:26 +0800

    Add league rank component

--- components/league-rank.js
+++ components/league-rank.js
@@ -14,56 +14,70 @@ const cardType = {
 }
 
 const coinType = {
-  "Gold": goldCoin,
-  "Silver": silverCoin,
+  Gold: goldCoin,
+  Silver: silverCoin,
 }
 
 export default function LeagueRank(props) {
+
+  const rank = Object.keys(props.rank)
+
   return (
-    <div className="flex items-center justify-between border-t last:border-b border-translucent mx-16 px-6">
+    <div className="flex items-center justify-between border-t last:border-b border-translucent mx-16 px-8">
       <div className="flex flex-col gap-1 text-lg">
-        RANK {props.rank + 1}
+        RANK {rank}
         <div className="flex gap-3">
-          <div className="bg-teal-400 rounded-full w-3 h-3" />
-          <div className="bg-teal-400 rounded-full w-3 h-3" />
-          <div className="border-2 border-teal-400 rounded-full w-3 h-3" />
-          <div className="border-2 border-teal-400 rounded-full w-3 h-3" />
+          {Array.from(Array(props.rank[rank].progress).keys()).map(item =>
+            <div key={item} className="bg-teal-400 rounded-full w-3 h-3" />
+          )}
+          {Array.from(Array(4 - props.rank[rank].progress).keys()).map(item =>
+            <div key={item} className="border-2 border-teal-400 rounded-full w-3 h-3" />
+          )}
         </div>
       </div>
       <div className="flex items-center gap-10">
-        <div
-          className="flex flex-row justify-center items-center gap-2">
-          <Image
-            src={silverCoin}
-            width={28}
-            height={28}
-            alt="Silver "
-            draggable="false"
-          />
-          1,200
-        </div>
-        <div
-          className="flex flex-row justify-center items-center gap-2">
-          <Image
-            src={goldCoin}
-            width={28}
-            height={28}
-            alt="Gold "
-            draggable="false"
-          />
-          4
-        </div>
-        <div
-          // title={reward.amount + " random card/s"}
-          className="flex flex-col justify-center items-center gap-1 select-none bg-translucent rounded-full p-1">
-          <Image
-            src={cardLayer2}
-            width={48}
-            height={48}
-            alt=""
-            draggable="false"
-          />
-        </div>
+        {
+          props.rank[rank].rewards.silver !== "0" &&
+          <div
+            className="flex flex-row justify-center items-center gap-2">
+            <Image
+              src={silverCoin}
+              width={28}
+              height={28}
+              alt="Silver "
+              draggable="false"
+            />
+            {props.rank[rank].rewards.silver}
+          </div>
+        }
+        {
+          props.rank[rank].rewards.gold !== "0" &&
+          <div
+            className="flex flex-row justify-center items-center gap-2">
+            <Image
+              src={goldCoin}
+              width={28}
+              height={28}
+              alt="Gold "
+              draggable="false"
+            />
+            {props.rank[rank].rewards.gold}
+          </div>
+        }
+        {
+          props.rank[rank].rewards.cards !== "0" &&
+          <div
+            title={props.rank[rank].rewards.cards + " random card/s"}
+            className="flex flex-col justify-center items-center gap-1 select-none bg-translucent rounded-full p-1">
+            <Image
+              src={cardType[props.rank[rank].rewards.cards]}
+              width={48}
+              height={48}
+              alt=""
+              draggable="false"
+            />
+          </div>
+        }
       </div>
     </div>
   )
--- pages/ranked.js
+++ pages/ranked.js
@@ -15,7 +15,40 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
 import streakFire from '../public/assets/icons/streak-fire.svg'
 
-export default function Campaign() {
+export default function Ranked() {
+
+  const [currentLeague, setCurrentLeague] = useState(0)
+
+  let leagues = [
+    {
+      name: "Bronze League",
+      ranks: [
+        { 1: { progress: 0, rewards: { silver: "1,500", gold: "2", cards: "2" } } },
+        { 2: { progress: 0, rewards: { silver: "1,200", gold: "1", cards: "2" } } },
+        { 3: { progress: 2, rewards: { silver: "1,000", gold: "1", cards: "1" } } },
+        { 4: { progress: 4, rewards: { silver: "800", gold: "0", cards: "1" } } },
+      ],
+    },
+    {
+      name: "Silver League",
+      ranks: [
+        { 1: { progress: 0, rewards: { silver: "2,000", gold: "3", cards: "3" } } },
+        { 2: { progress: 0, rewards: { silver: "1,800", gold: "2", cards: "2" } } },
+        { 3: { progress: 0, rewards: { silver: "1,300", gold: "2", cards: "2" } } },
+        { 4: { progress: 0, rewards: { silver: "1,000", gold: "1", cards: "1" } } },
+      ],
+    },
+    {
+      name: "Gold League",
+      ranks: [
+        { 1: { progress: 0, rewards: { silver: "2,300", gold: "3", cards: "3" } } },
+        { 2: { progress: 0, rewards: { silver: "2,000", gold: "2", cards: "2" } } },
+        { 3: { progress: 0, rewards: { silver: "1,600", gold: "2", cards: "2" } } },
+        { 4: { progress: 0, rewards: { silver: "1,200", gold: "2", cards: "2" } } },
+      ],
+    },
+  ]
+
   return (
     <div>
       <Head>
@@ -62,18 +95,18 @@ export default function Campaign() {
         <div className="flex flex-col items-center bg-translucent rounded-2xl shadow-md w-3/5 m-10 ml-5">
           <div className="flex justify-between text-2xl drop-shadow-md whitespace-nowrap w-full px-36 my-6">
             <button
-              // disabled={currentRealm === 0 ? true : false}
-              // onClick={() => setCurrentRealm(currentRealm - 1)}
+              disabled={currentLeague === 0 ? true : false}
+              onClick={() => setCurrentLeague(currentLeague - 1)}
               className="flex justify-start items-center hover:text-teal-300 w-full py-5 disabled:text-white/60">
               <FontAwesomeIcon
                 icon={faChevronLeft}
                 draggable="false"
               />
             </button>
-            <div className="flex items-center select-none uppercase pt-1">{/*realms[currentRealm].name*/}SILVER LEAGUE</div>
+            <div className="flex items-center select-none uppercase pt-1">{leagues[currentLeague].name}</div>
             <button
-              // disabled={currentRealm === realms.length - 1 ? true : false}
-              // onClick={() => setCurrentRealm(currentRealm + 1)}
+              disabled={currentLeague === leagues.length - 1 ? true : false}
+              onClick={() => setCurrentLeague(currentLeague + 1)}
               className="flex justify-end items-center hover:text-teal-300 w-full py-5 disabled:text-white/60">
               <FontAwesomeIcon
                 icon={faChevronLeft}
@@ -85,7 +118,7 @@ export default function Campaign() {
           <div className="grow w-full">
             <div className="grid grid-flow-row h-full">
               {Array.from(Array(4).keys()).map(item =>
-                <LeagueRank key={item} rank={item} />
+                <LeagueRank key={item} rank={leagues[currentLeague].ranks[item]} />
               )}
             </div>
           </div>
@@ -99,6 +132,6 @@ export default function Campaign() {
           </div>
         </div>
       </main>
-    </div>
+    </div >
   )
 }
