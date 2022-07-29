38133e08ca329aaf49dd2bfadb5040060114a47e
teddzyb <teddybartletg88@gmail.com>
2022-07-29 15:11:34 +0800

    Add initial practice menu layout

--- pages/practice.js
+++ pages/practice.js
@@ -1,5 +1,15 @@
+// APIs
 import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+
+// Components
 import TopMenuBar from '../components/top-menu-bar'
+
+// Assets
+import classicMode from '../public/assets/icons/mode-classic.svg'
+import tutorialMode from '../public/assets/icons/mode-tutorial.svg'
+import rushMode from '../public/assets/icons/mode-rush.svg'
 
 export default function Practice() {
   return (
@@ -12,7 +22,55 @@ export default function Practice() {
       <nav className="sticky top-0 z-50">
         <TopMenuBar />
       </nav>
-      <main className="h-screen flex flex-row justify-center pb-20">
+      <main className="h-screen flex flex-col justify-center items-center gap-28 pb-20">
+        <div className="flex gap-24">
+          <div className="flex flex-col items-center shadow-md rounded-xl bg-translucent w-60 h-80">
+            <div className="flex justify-center h-full">
+              <Image
+                src={tutorialMode}
+                width={72}
+                height={72}
+                alt=""
+                className="drop-shadow-md"
+                draggable="false"
+              />
+            </div>
+            <div className="text-lg drop-shadow-md pb-5 -mt-2">TUTORIAL</div>
+          </div>
+          <div className="flex flex-col items-center shadow-md rounded-xl ring-4 ring-teal-400 bg-translucent w-60 h-80 scale-110">
+            <div className="flex justify-center h-full">
+              <Image
+                src={classicMode}
+                width={72}
+                height={72}
+                alt=""
+                className="drop-shadow-md"
+                draggable="false"
+              />
+            </div>
+            <div className="text-lg drop-shadow-md pb-5 -mt-2">CLASSIC</div>
+          </div>
+          <div className="flex flex-col items-center shadow-md rounded-xl bg-translucent w-60 h-80">
+            <div className="flex justify-center h-full">
+              <Image
+                src={rushMode}
+                width={72}
+                height={72}
+                alt=""
+                className="drop-shadow-md"
+                draggable="false"
+              />
+            </div>
+            <div className="text-lg drop-shadow-md pb-5 -mt-2">RUSH</div>
+          </div>
+        </div>
+        <div>
+          <Link href="/menu">
+            <a>
+              <button className="btn-primary h-11 w-40">BACK</button>
+            </a>
+          </Link>
+        </div>
       </main>
     </div>
   )
