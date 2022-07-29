1fe86f91381eb9b67a9a6704ab1fd2b713e99c74
teddzyb <teddybartletg88@gmail.com>
2022-07-29 20:46:30 +0800

    Add initial custom menu layout

--- pages/custom.js
+++ pages/custom.js
@@ -10,7 +10,7 @@ import Card from '../components/card'
 
 // Assets
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCaretDown, faCirclePlus } from '@fortawesome/free-solid-svg-icons'
+import { faAngleDown, faCirclePlus } from '@fortawesome/free-solid-svg-icons'
 
 export default function Ranked() {
   return (
@@ -24,6 +24,31 @@ export default function Ranked() {
         <TopMenuBar />
       </nav>
       <main className="h-screen flex flex-row justify-center pb-20">
+        <div className="rounded-xl shadow-md bg-translucent overflow-hidden w-72 m-10 mr-0">
+          <div className="text-center select-none bg-translucent w-full pb-2 pt-3">GAME OPTIONS</div>
+          <div className="scrollbar-thin scrollbar-thumb-translucent scrollbar-track-transparent">
+
+          </div>
+        </div>
+        <div className="flex flex-col grow my-10">
+          <div className="grow">
+
+          </div>
+          <div className="flex justify-center gap-8 w-full my-10">
+            <Link href="/menu">
+              <a draggable="false">
+                <button className="btn-primary h-11 w-40">BACK</button>
+              </a>
+            </Link>
+            <button className="btn-primary h-11 w-40">START</button>
+          </div>
+        </div>
+        <div className="rounded-xl shadow-md bg-translucent overflow-hidden w-72 m-10 ml-0">
+          <div className="text-center select-none bg-translucent w-full pb-2 pt-3">LOBBY</div>
+          <div className="p-4 pt-3 scrollbar-thin scrollbar-thumb-translucent scrollbar-track-transparent">
+
+          </div>
+        </div>
       </main>
     </div>
   )
