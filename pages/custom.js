0abb36aa3d57b92864f3b0cf486aef1b829eeb15
teddzyb <teddybartletg88@gmail.com>
2022-07-30 20:06:27 +0800

    Add todo comment for chat feature

--- pages/custom.js
+++ pages/custom.js
@@ -78,9 +78,9 @@ export default function Ranked() {
             <button className="btn-primary h-11 w-40">START</button>
           </div>
         </div>
-        <div className="rounded-xl shadow-md bg-translucent overflow-hidden w-72 m-10 ml-0">
+        <div className="flex flex-col rounded-xl shadow-md bg-translucent overflow-hidden w-72 m-10 ml-0">
           <div className="text-center select-none bg-translucent w-full pb-2 pt-3">ROOM 00001</div>
-          <div className="flex flex-col gap-2 border-b border-translucent p-4 scrollbar-thin scrollbar-thumb-translucent scrollbar-track-transparent">
+          <div className="border-b border-translucent p-4">
             <div className="grid grid-cols-4 gap-y-2 items-center">
               <label htmlFor="joinCode" className="text-sm whitespace-nowrap mr-3 pt-1">JOIN</label>
               <div className="flex col-span-3">
@@ -120,7 +120,7 @@ export default function Ranked() {
               </div>
             </div>
           </div>
-          <div className="p-4">
+          <div className="p-4 scrollbar-thin scrollbar-thumb-translucent scrollbar-track-transparent">
             <div className="text-sm pb-1">PLAYERS (2/2)</div>
             <div className="flex items-center gap-4 rounded-md p-2 hover:bg-translucent/10">
               <div className="flex rounded-md outline outline-2 outline-translucent shadow-sm w-fit">
@@ -204,6 +204,10 @@ export default function Ranked() {
               </div>
             </div>
           </div>
+          {/* TODO: Chat feature */}
+          {/* <div className="text-sm border-t border-translucent drop-shadow-md max-h-80 h-60 p-4">
+            CHAT
+          </div> */}
         </div>
       </main>
     </div>
