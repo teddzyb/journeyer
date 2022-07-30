4bbf3f99bc047c714aded58b2be1b0aa5a879eb5
teddzyb <teddybartletg88@gmail.com>
2022-07-30 13:31:08 +0800

    Add preset buttons

--- pages/custom.js
+++ pages/custom.js
@@ -12,7 +12,14 @@ import Card from '../components/card'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleDown, faCirclePlus } from '@fortawesome/free-solid-svg-icons'
 
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ")
+}
+
 export default function Ranked() {
+
+  const [currentMode, setCurrentMode] = useState(0)
+
   return (
     <div>
       <Head>
@@ -26,8 +33,36 @@ export default function Ranked() {
       <main className="h-screen flex flex-row justify-center pb-20">
         <div className="rounded-xl shadow-md bg-translucent overflow-hidden w-72 m-10 mr-0">
           <div className="text-center select-none bg-translucent w-full pb-2 pt-3">GAME OPTIONS</div>
-          <div className="scrollbar-thin scrollbar-thumb-translucent scrollbar-track-transparent">
-
+          <div className="p-4 pt-3 scrollbar-thin scrollbar-thumb-translucent scrollbar-track-transparent">
+            <div className="grid grid-cols-2 gap-2 border-b border-translucent pb-4">
+              <button
+                onClick={() => setCurrentMode(0)}
+                className={classNames(
+                  "text-sm rounded-lg p-2 pt-3",
+                  currentMode === 0 ? "text-black bg-teal-500" : "text-teal-50 bg-translucent"
+                )}
+              >
+                CLASSIC
+              </button>
+              <button
+                onClick={() => setCurrentMode(1)}
+                className={classNames(
+                  "text-sm rounded-lg p-2 pt-3",
+                  currentMode === 1 ? "text-black bg-teal-500" : "text-teal-50 bg-translucent"
+                )}
+              >
+                RUSH
+              </button>
+              <button
+                onClick={() => setCurrentMode(2)}
+                className={classNames(
+                  "text-sm rounded-lg p-2 pt-3 col-span-2",
+                  currentMode === 2 ? "text-black bg-teal-500" : "text-teal-50 bg-translucent"
+                )}
+              >
+                CUSTOM
+              </button>
+            </div>
           </div>
         </div>
         <div className="flex flex-col grow my-10">
@@ -44,7 +79,7 @@ export default function Ranked() {
           </div>
         </div>
         <div className="rounded-xl shadow-md bg-translucent overflow-hidden w-72 m-10 ml-0">
-          <div className="text-center select-none bg-translucent w-full pb-2 pt-3">LOBBY</div>
+          <div className="text-center select-none bg-translucent w-full pb-2 pt-3">ROOM 00001</div>
           <div className="p-4 pt-3 scrollbar-thin scrollbar-thumb-translucent scrollbar-track-transparent">
 
           </div>
