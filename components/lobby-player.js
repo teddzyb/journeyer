0dcc6633d7e764fdd1ea62af0d2371342ecbca29
teddzyb <teddybartletg88@gmail.com>
2022-08-04 00:51:30 +0800

    Add icons to context menu

--- components/lobby-player.js
+++ components/lobby-player.js
@@ -1,40 +1,71 @@
 import Image from 'next/future/image'
 import { motion } from 'framer-motion'
+import * as ContextMenu from '@radix-ui/react-context-menu'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { faUser, faUserPlus, faCircleMinus } from '@fortawesome/free-solid-svg-icons'
 
 export default function LobbyPlayer(props) {
   return props.type !== "placeholder" ?
     (
-      <motion.div
-        key={props.player.key}
-        drag="y"
-        dragSnapToOrigin
-        dragElastic={1}
-        onDragStart={() => props.setDragging(true)}
-        onDragEnd={(event, info) => {
-          props.setDragging(false)
-          if (props.type === "spectator" && props.playerCount >= 2)
-            return
-          props.overlap(event, info, props.listRef) &&
-            props.handleLobby.setItemProp(props.player.key, "role", (props.type === "player" ? "spectator" : "player"))
-        }}
-        className="flex items-center gap-4 rounded-md p-2 hover:bg-translucent/10 hover:shadow-md">
-        <div className="flex rounded-md outline outline-2 outline-translucent shadow-sm w-fit">
-          <Image
-            src={props.player.avatar}
-            height={48}
-            width={48}
-            className="rounded-md"
-            alt=""
-            draggable="false"
-          />
-        </div>
-        <div className="flex flex-col pt-[2px] gap-[2px]">
-          <div>{props.player.username}</div>
-          <div className="text-sm">LEVEL {props.player.level}</div>
-        </div>
-      </motion.div>
+      <ContextMenu.Root>
+        <ContextMenu.Trigger>
+          <motion.div
+            key={props.player.key}
+            drag="y"
+            dragSnapToOrigin
+            dragElastic={1}
+            onDragStart={() => props.setDragging(true)}
+            onDragEnd={(event, info) => {
+              props.setDragging(false)
+              if (props.type === "spectator" && props.playerCount >= 2)
+                return
+              props.overlap(event, info, props.listRef) &&
+                props.handleLobby.setItemProp(props.player.key, "role", (props.type === "player" ? "spectator" : "player"))
+            }}
+            className="flex items-center gap-4 rounded-md p-2 hover:bg-translucent/10 hover:shadow-md">
+            <div className="flex rounded-md outline outline-2 outline-translucent shadow-sm w-fit">
+              <Image
+                src={props.player.avatar}
+                height={48}
+                width={48}
+                className="rounded-md"
+                alt=""
+                draggable="false"
+              />
+            </div>
+            <div className="flex flex-col pt-[2px] gap-[2px]">
+              <div>{props.player.username}</div>
+              <div className="text-sm">LEVEL {props.player.level}</div>
+            </div>
+          </motion.div>
+        </ContextMenu.Trigger>
+
+        <ContextMenu.Portal>
+          <ContextMenu.Content className="flex flex-col gap-1 bg-[#0c4441] rounded-md pt-[10px] pb-2 px-2">
+            <ContextMenu.Item className="hover:outline-none hover:text-teal-300 px-1">
+              <button className="flex items-center gap-2 text-start w-full">
+                <FontAwesomeIcon
+                  icon={faUserPlus}
+                  draggable="false"
+                  className="aspect-square pb-[2px]"
+                />
+                <div>Add to Friends</div>
+              </button>
+            </ContextMenu.Item>
+            <ContextMenu.Separator className="h-px bg-translucent" />
+            <ContextMenu.Item className="text-red-300 hover:outline-none hover:text-red-400 px-1">
+              <button className="flex items-center gap-2 text-start w-full">
+                <FontAwesomeIcon
+                  icon={faCircleMinus}
+                  draggable="false"
+                  className="aspect-square pb-[2px]"
+                />
+                <div>Remove</div>
+              </button>
+            </ContextMenu.Item>
+          </ContextMenu.Content>
+        </ContextMenu.Portal>
+      </ContextMenu.Root>
     ) : (
       <div className="flex items-center gap-4 rounded-md select-none p-2">
         <div className="flex rounded-md outline-dashed outline-2 outline-translucent/25 overflow-hidden w-fit">
