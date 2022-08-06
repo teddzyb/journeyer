3dc5fdb66be29fb8887426d423a472e428599e7a
teddzyb <teddybartletg88@gmail.com>
2022-08-06 23:03:29 +0800

    Opimizations

--- components/friends.js
+++ components/friends.js
@@ -3,8 +3,8 @@ import { faXmark } from '@fortawesome/free-solid-svg-icons'
 
 export default function Friends(props) {
   return (
-    <div className="rounded-lg bg-teal-900 border border-translucent shadow-md h-96">
-      <div className="flex justify-between items-center border-b border-translucent px-4 py-3">
+    <div className="rounded-lg bg-teal-900 border border-translucent shadow-md overflow-hidden">
+      <div className="sticky top-0 flex justify-between items-center border-b border-translucent backdrop-blur-md px-4 py-3">
         <div className="text-lg pt-1 pl-1">FRIENDS</div>
         <button className="flex" onClick={() => props.setFriends(false)}>
           <FontAwesomeIcon
@@ -13,6 +13,11 @@ export default function Friends(props) {
             draggable="false"
           />
         </button>
+      </div>
+      <div className="scrollbar-thin scrollbar-thumb-translucent scrollbar-track-transparent max-h-96">
+        <div className="flex justify-center items-center h-24">
+          You have no friends... Go make some!
+        </div>
       </div>
     </div>
   )
