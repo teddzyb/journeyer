73b0eeb1e80a550a85da64580ab5b3d3cca39dae
teddzyb <teddybartletg88@gmail.com>
2022-08-07 14:16:58 +0800

    Add notifications component design

--- components/notifications.js
+++ components/notifications.js
@@ -1,10 +1,22 @@
-export default function Notifications() {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPaperPlane, faUserMinus } from '@fortawesome/free-solid-svg-icons'
+import Image from 'next/image'
+
+export default function Notifications(props) {
   return (
-    <div className="rounded-lg bg-teal-900 border border-translucent shadow-md p-4">
-      notifications<br />
-      notifications<br />
-      notifications<br />
-      notifications<br />
+    <div className="rounded-lg select-none bg-teal-900 border border-translucent shadow-md overflow-hidden w-96">
+      <div className="sticky top-0 flex justify-between items-center border-b border-translucent px-4 py-3">
+        <div className="flex gap-4 text-lg pt-1 pl-1">
+          <button className="text-teal-300">NOTIFICATIONS</button>
+          <button className="text-white/60">NEWS</button>
+        </div>
+        {props.children}
+      </div>
+      <div className="scrollbar-thin scrollbar-thumb-translucent scrollbar-track-transparent max-h-96">
+        <div className="flex justify-center items-center h-24">
+          Nothing here yet...
+        </div>
+      </div>
     </div>
   )
 }
