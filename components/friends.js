b1d15333d6607b79e5cb740cefc6aa50706175e7
teddzyb <teddybartletg88@gmail.com>
2022-08-07 22:04:14 +0800

    Add tooltips to friends component

--- components/friends.js
+++ components/friends.js
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import * as Tabs from '@radix-ui/react-tabs'
+import * as Tooltip from '@radix-ui/react-tooltip'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPaperPlane, faUserMinus, faCheck, faXmark } from '@fortawesome/free-solid-svg-icons'
 
@@ -50,20 +51,40 @@ export default function Friends(props) {
                   <div className="text-sm">LEVEL 25</div>
                 </div>
                 <div className="ml-auto flex gap-2 invisible group-hover:visible mx-2">
-                  <button className="flex">
-                    <FontAwesomeIcon
-                      icon={faPaperPlane}
-                      className="text-md bg-translucent/10 hover:bg-translucent aspect-square rounded-full p-2.5 shadow-sm"
-                      draggable="false"
-                    />
-                  </button>
-                  <button className="flex">
-                    <FontAwesomeIcon
-                      icon={faUserMinus}
-                      className="text-md bg-translucent/10 hover:bg-translucent aspect-square rounded-full p-2.5 shadow-sm"
-                      draggable="false"
-                    />
-                  </button>
+                  <Tooltip.Provider delayDuration="750">
+                    <Tooltip.Root>
+                      <Tooltip.Trigger asChild>
+                        <button className="flex">
+                          <FontAwesomeIcon
+                            icon={faPaperPlane}
+                            className="text-md bg-translucent/10 hover:bg-translucent aspect-square rounded-full p-2.5 shadow-sm"
+                            draggable="false"
+                          />
+                        </button>
+                      </Tooltip.Trigger>
+                      <Tooltip.Content side="bottom" className="bg-[#0e3c38] rounded-md py-1 pt-[6px] px-3">
+                        Invite
+                        <Tooltip.Arrow className="fill-[#0e3c38] mb-1" />
+                      </Tooltip.Content>
+                    </Tooltip.Root>
+                  </Tooltip.Provider>
+                  <Tooltip.Provider delayDuration="750">
+                    <Tooltip.Root>
+                      <Tooltip.Trigger asChild>
+                        <button className="flex">
+                          <FontAwesomeIcon
+                            icon={faUserMinus}
+                            className="text-md bg-translucent/10 hover:bg-translucent aspect-square rounded-full p-2.5 shadow-sm"
+                            draggable="false"
+                          />
+                        </button>
+                      </Tooltip.Trigger>
+                      <Tooltip.Content side="bottom" className="bg-[#0e3c38] rounded-md py-1 pt-[6px] px-3">
+                        Remove Friend
+                        <Tooltip.Arrow className="fill-[#0e3c38] mb-1" />
+                      </Tooltip.Content>
+                    </Tooltip.Root>
+                  </Tooltip.Provider>
                 </div>
               </div>
             )}
@@ -85,13 +106,23 @@ export default function Friends(props) {
                   <div className="text-sm">LEVEL 25</div>
                 </div>
                 <div className="ml-auto flex gap-2 invisible group-hover:visible mx-2">
-                  <button className="flex">
-                    <FontAwesomeIcon
-                      icon={faUserMinus}
-                      className="text-md bg-translucent/10 hover:bg-translucent aspect-square rounded-full p-2.5 shadow-sm"
-                      draggable="false"
-                    />
-                  </button>
+                  <Tooltip.Provider delayDuration="750">
+                    <Tooltip.Root>
+                      <Tooltip.Trigger asChild>
+                        <button className="flex">
+                          <FontAwesomeIcon
+                            icon={faUserMinus}
+                            className="text-md bg-translucent/10 hover:bg-translucent aspect-square rounded-full p-2.5 shadow-sm"
+                            draggable="false"
+                          />
+                        </button>
+                      </Tooltip.Trigger>
+                      <Tooltip.Content side="bottom" className="bg-[#0e3c38] rounded-md py-1 pt-[6px] px-3">
+                        Remove Friend
+                        <Tooltip.Arrow className="fill-[#0e3c38] mb-1" />
+                      </Tooltip.Content>
+                    </Tooltip.Root>
+                  </Tooltip.Provider>
                 </div>
               </div>
             )}
@@ -139,20 +170,40 @@ export default function Friends(props) {
                   <div className="text-sm">LEVEL 25</div>
                 </div>
                 <div className="ml-auto flex gap-2 invisible group-hover:visible mx-2">
-                  <button className="flex">
-                    <FontAwesomeIcon
-                      icon={faCheck}
-                      className="text-md bg-translucent/10 hover:bg-translucent aspect-square rounded-full p-2.5 shadow-sm"
-                      draggable="false"
-                    />
-                  </button>
-                  <button className="flex">
-                    <FontAwesomeIcon
-                      icon={faXmark}
-                      className="text-md bg-translucent/10 hover:bg-translucent aspect-square rounded-full p-2.5 shadow-sm"
-                      draggable="false"
-                    />
-                  </button>
+                  <Tooltip.Provider delayDuration="1000">
+                    <Tooltip.Root>
+                      <Tooltip.Trigger asChild>
+                        <button className="flex">
+                          <FontAwesomeIcon
+                            icon={faCheck}
+                            className="text-md bg-translucent/10 hover:bg-translucent aspect-square rounded-full p-2.5 shadow-sm"
+                            draggable="false"
+                          />
+                        </button>
+                      </Tooltip.Trigger>
+                      <Tooltip.Content side="bottom" className="bg-[#0e3c38] rounded-md py-1 pt-[6px] px-3">
+                        Accept
+                        <Tooltip.Arrow className="fill-[#0e3c38] mb-1" />
+                      </Tooltip.Content>
+                    </Tooltip.Root>
+                  </Tooltip.Provider>
+                  <Tooltip.Provider delayDuration="1000">
+                    <Tooltip.Root>
+                      <Tooltip.Trigger asChild>
+                        <button className="flex">
+                          <FontAwesomeIcon
+                            icon={faXmark}
+                            className="text-md bg-translucent/10 hover:bg-translucent aspect-square rounded-full p-2.5 shadow-sm"
+                            draggable="false"
+                          />
+                        </button>
+                      </Tooltip.Trigger>
+                      <Tooltip.Content side="bottom" className="bg-[#0e3c38] rounded-md py-1 pt-[6px] px-3">
+                        Decline
+                        <Tooltip.Arrow className="fill-[#0e3c38] mb-1" />
+                      </Tooltip.Content>
+                    </Tooltip.Root>
+                  </Tooltip.Provider>
                 </div>
               </div>
             )}
