c59bd2a21d314aa7c8321d92a39fb35a50248fd7
teddzyb <teddybartletg88@gmail.com>
2022-08-10 13:15:21 +0800

    Add icons (settings, surrender, etc.)

--- pages/match/[match-id].js
+++ pages/match/[match-id].js
@@ -1,18 +1,26 @@
 // APIs
 import Head from 'next/head'
+import { useState } from 'react'
 
 // Components
 import Card from '../../components/card'
 
 // Assets
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUserPlus, faCommentDots } from '@fortawesome/free-solid-svg-icons'
+import { faGear, faFlag, faVolumeHigh, faVolumeXmark, faQuestionCircle, faUserPlus, faCommentDots }
+  from '@fortawesome/free-solid-svg-icons'
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ")
 }
 
 export default function Match() {
+
+  const [volume, setVolume] = useState(false)
+  const toggleVolume = () => setVolume(!volume)
+
+  let currentVolume = volume ? faVolumeXmark : faVolumeHigh
+
   return (
     <div>
       <Head>
@@ -21,7 +29,40 @@ export default function Match() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="h-screen flex flex-row justify-center px-20 py-12">
-        <div className="bg-translucent rounded-2xl shadow-md h-full w-full"></div>
+        <div className="flex flex-col items-center gap-3 absolute right-5 top-5">
+          <button className="flex">
+            <FontAwesomeIcon
+              icon={faGear}
+              className="text-2xl bg-translucent/10 hover:bg-translucent aspect-square rounded-full p-2 shadow-sm"
+              draggable="false"
+            />
+          </button>
+          <button className="flex">
+            <FontAwesomeIcon
+              icon={currentVolume}
+              onClick={toggleVolume}
+              className="text-2xl bg-translucent/10 hover:bg-translucent aspect-square rounded-full p-2 shadow-sm"
+              draggable="false"
+            />
+          </button>
+          <button className="flex">
+            <FontAwesomeIcon
+              icon={faQuestionCircle}
+              className="text-2xl bg-translucent/10 hover:bg-translucent aspect-square rounded-full p-2 shadow-sm"
+              draggable="false"
+            />
+          </button>
+          <button className="flex">
+            <FontAwesomeIcon
+              icon={faFlag}
+              className="text-xl -rotate-12 bg-translucent/10 hover:bg-translucent aspect-square rounded-full p-2.5 shadow-sm"
+              draggable="false"
+            />
+          </button>
+        </div>
+        <div className="bg-translucent rounded-2xl shadow-md h-full w-full">
+
+        </div>
       </main>
     </div>
   )
